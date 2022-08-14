f18a24af33eb015b03e062683eaadc8d54872a56
Akhundov Vahid <vaha.az@gmail.com>
2022-08-14 21:15:16 +0200

    Exercise Tracker (Test)

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/api/users/:_id/logs', async (req, res) => {
   user.exercises = req.query.from ? user.exercises.filter(el => (new Date(el.date)).getTime() > (new Date(req.query.from)).getTime()) : user.exercises;
   user.exercises = req.query.from ? user.exercises.filter(el => (new Date(el.date)).getTime() < (new Date(req.query.to)).getTime()) : user.exercises;
 
-  res.json({username: user.username, count: user.exercises.length, _id: user._id, log: user.exercises.slice(0, req.query.limit).map(el => {el.date = (new Date(el.date)).toDateString()})});
+  res.json({username: user.username, count: user.exercises.length, _id: user._id, log: user.exercises.slice(0, req.query.limit).map(el => {el.date = (new Date(el.date)).toDateString(); return el})});
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
