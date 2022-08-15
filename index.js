d3a77c9bcfaec96eec78a1db406037b988548add
Akhundov Vahid <vaha.az@gmail.com>
2022-08-15 21:22:03 +0200

    Exercise Tracker (Test)

--- index.js
+++ index.js
@@ -55,11 +55,8 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
 
 app.get('/api/users/:_id/logs', async (req, res) => {
   let user = await User.findById(req.params._id);
-  console.log(await Exercise.find({}));
-  let fort = 0;
-  user.exercises = req.query.from ? user.exercises.filter(el => (new Date(el.date)).getTime() > (new Date(req.query.from)).getTime()) : user.exercises;
-  user.exercises = req.query.from ? user.exercises.filter(el => (new Date(el.date)).getTime() < (new Date(req.query.to)).getTime()) : user.exercises;
-  var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+  user.exercises = req.query.from ? user.exercises.filter(el => el.date.getTime() > req.query.from.getTime()) : user.exercises;
+  user.exercises = req.query.from ? user.exercises.filter(el => el.date.getTime() < req.query.to.getTime()) : user.exercises;
   res.json({username: user.username, count: user.exercises.length, _id: user._id, log: user.exercises.slice(0, req.query.limit).map(el => ({date: el.date.toDateString(), duration: el.duration, description:el.description}))});
 });
 
